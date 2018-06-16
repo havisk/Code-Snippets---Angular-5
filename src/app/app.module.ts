97094e11034231c8c2c6c9bf0e922a8515c293bb
Kool Havis <havisk@gmail.com>
2018-06-16 14:59:14 -0400

    set up structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {HomePageControllerComponent} from "./home-page/home-page-controller/home-page-controller.component";
+import {HomePageModule} from "./home-page/home-page.module";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      FlexLayoutModule,
+      HomePageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
