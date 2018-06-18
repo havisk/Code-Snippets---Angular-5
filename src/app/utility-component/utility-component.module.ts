67b3bde04c440a689947f823f9637f53421a0d91
Kool Havis <havisk@gmail.com>
2018-06-17 20:42:22 -0400

    created and styled modal window

---
+++ src/app/utility-component/utility-component.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalWindowComponent } from './modal-window/modal-window.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+
+@NgModule({
+  imports: [
+      CommonModule,
+      FlexLayoutModule
+  ],
+  exports: [
+      ModalWindowComponent
+  ],
+  declarations: [ModalWindowComponent]
+})
+export class UtilityComponentModule { }
