ba2bc85fc628aca76552bcbeef5336e34487e54b
Kool Havis <havisk@gmail.com>
2018-06-17 23:49:16 -0400

    setup snippet card

---
+++ src/app/home-page/models/temp-languages.enum.ts
@@ -0,0 +1,6 @@
+import {Snippet} from './snippet';
+
+export enum Languages {
+    typscript = 'TypeScript',
+    css = 'CSS'
+}
--- src/app/utility-component/modal-window/modal-window.component.ts
+++ src/app/utility-component/modal-window/modal-window.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalWindowComponent implements OnInit {
 
+  showing = false;
+
   constructor() { }
 
   ngOnInit() {
